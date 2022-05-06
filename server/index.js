f0c253367edb4db6bf304377e8fd8a6b6b880408
vaishnavihole <vaishnavihole1@gmail.com>
2022-05-06 20:38:01 +0530

    env added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
+mongoose.connect(process.env.MONGO_URI,
 {useNewUrlParser: true,
 useUnifiedTopology: true}, () => {
 console.log('Connected to mongoDB')
